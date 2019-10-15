e79a98c9973668cf6f33392b8ffd357de24b7d6d
jina <junjina63@naver.com>
2019-10-15 14:46:32 +0900

    fix main

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -344,7 +344,7 @@
 	           	$("#create-d-day").flatpickr({
         		    dateFormat: "Y.m.d"
         		});
-	           	map_create = L.map('mapid_create').setView([37.566, 126.978], 14);
+	           	map_create = L.map('mapid_create').setView([37.566, 126.978], 5);
 	    		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	    			maxZoom: 18,
 	    			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
